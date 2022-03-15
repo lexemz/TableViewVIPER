bc86085ee8636433cc5a3dc6fdb2eef6b7149ba9
Igor Buzykin <lexemz@gmail.com>
2022-03-15 15:56:04 +0300

    Initial Commit

---
+++ TableViewVIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewVIPER
+//
+//  Created by Igor on 15.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
